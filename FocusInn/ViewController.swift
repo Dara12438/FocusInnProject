0747922f0edd573802b53d4576c5770915c9d40e
Dara Oseyemi <s012438@lm-20s012438.lmsd.org>
2019-01-09 15:24:13 -0500

    Initial Commit

---
+++ FocusInn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FocusInn
+//
+//  Created by Dara Oseyemi (student LM) on 1/9/19.
+//  Copyright © 2019 Dara Oseyemi (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
